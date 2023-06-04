d92adacafc94a2c54625eee96a54565b617ba57e
Rohit T P <tprohit9@gmail.com>
2023-06-04 22:46:34 +0530

    auto commit

--- main.cpp
+++ main.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 #include "BigInt.h"
 
-int main() {
-    BigInt a;
-    BigInt b;
+#define cube(x) (x * x * x)
 
-    std::cin >> a >> b;
-    std::cout << a + b << std::endl;
-    std::cout << (a == b) << std::endl;
+int main() {
+//    BigInt a;
+//    BigInt b;
+//
+//    std::cin >> a >> b;
+//    std::cout << a + b << std::endl;
+//    std::cout << (a == b) << std::endl;
+
+    int a = 2;
+
+    a = cube(++a);
+
+    std::cout << a ;
 
     return 0;
 }
